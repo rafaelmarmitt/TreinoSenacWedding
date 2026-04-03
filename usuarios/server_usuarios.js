9770574ba0d8db0376456388381f235129b9b120
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-04-03 16:29:18 -0300

    Revert "a"
    This reverts commit 3a085388950a6cd64357a324895a6805f89161c6.

--- usuarios/server_usuarios.js
+++ usuarios/server_usuarios.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(express.json());
 
 const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'db_usuarios'
 };
 
 // ==========================================
