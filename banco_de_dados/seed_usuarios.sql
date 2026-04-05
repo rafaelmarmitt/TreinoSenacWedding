f0b340a6a1d31ba12af2e353a14679ad9d73b157
rafaelmarmitt <167102801+rafaelmarmitt@users.noreply.github.com>
2026-04-05 13:05:10 -0300

    a

--- banco_de_dados/seed_usuarios.sql
+++ banco_de_dados/seed_usuarios.sql
@@ -16,5 +16,5 @@ CREATE TABLE IF NOT EXISTS usuarios (
 -- A senha original para ambos é: 123456
 -- O hash abaixo ($2a$12$...) é a representação Bcrypt de "123456" gerado com salt rounds = 10
 INSERT INTO usuarios (nome, cpf, email, senha, perfil) VALUES
-('Administrador Geral', '111.111.111-11', 'admin@weddingpass.com', '$2a$10$Xm/A/.1nC3fT6lU9Z/Z8z.B2UqG1Fj4M1yCq1c5F9F1aZq1.5.Ww2', 'Admin'),
+('Administrador Geral', '111.111.111-11', 'admin@weddingpass.com', '$2a$10$Xm/A/.1nC3fT6lU9Z/Z8z.B2UqFj4G1M1yCq1c5F9F1aZq1.5.Ww2', 'Admin'),
 ('Maria Cerimonialista', '222.222.222-22', 'maria@weddingpass.com', '$2a$10$Xm/A/.1nC3fT6lU9Z/Z8z.B2UqG1Fj4M1yCq1c5F9F1aZq1.5.Ww2', 'Cerimonialista');
